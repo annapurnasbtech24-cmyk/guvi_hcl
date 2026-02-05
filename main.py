8d11afa9973163372151496a3d5de3e508b2eb39
annapurnasbtech24-cmyk <annapurnasbtech24@rvu.edu.in>
2026-02-05 22:30:15 +0530

    Refactor imports to use AutoProcessor and AutoModel

--- main.py
+++ main.py
@@ -60,12 +60,13 @@ DEVICE = 0 if torch.cuda.is_available() else -1
 # NOTE:
 # This model is multilingual and based on XLSR,
 # which supports Indian languages reasonably well.
-from transformers import Wav2Vec2Processor, Wav2Vec2Model
+from transformers import AutoProcessor, AutoModel
 
-processor = Wav2Vec2Processor.from_pretrained(
+processor = AutoProcessor.from_pretrained(
     "facebook/wav2vec2-base-xlsr-53"
 )
-model = Wav2Vec2Model.from_pretrained(
+
+model = AutoModel.from_pretrained(
     "facebook/wav2vec2-base-xlsr-53"
 )
 
